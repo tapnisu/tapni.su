8a91712ff60af0675103b91bdf219a14cfb6b98c
Alexiy Rybin <0xtapnisu@gmail.com>
2022-07-22 11:01:05 +0700

    Added more contacts

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,11 @@ const Footer = () => {
 		<>
 			<footer className="flex flex-row flex-wrap justify-between p-8">
 				<span className="uppercase">
-					<Link href="https://github.com/tapnisu">Github</Link>/
-					<Link href="https://t.me/AlexiyRybin">Telegram</Link>/
-					<Link href="https://twitter.com/tapnisu">Twitter</Link>
+					<Link href="https://github.com/tapnisu">Github</Link> / {""}
+					<Link href="https://t.me/AlexiyRybin">Telegram</Link> / {""}
+					<Link href="https://twitter.com/tapnisu">Twitter</Link> / {""}
+					<Link href="https://discord.com/users/586128640136445964">Discord</Link> / {""}
+					<Link href="mailto:alexiy.rybin@gmail.com">Email</Link>
 				</span>
 				Copyright © 2022 Alexiy Rybin.
 			</footer>
