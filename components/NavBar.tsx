97cd7a640bca6908125b8973350339f251c79c15
Alexiy Rybin <0xtapnisu@gmail.com>
2022-08-02 20:50:17 +0700

    Fixed images for navbar blur

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
 	return (
 		<>
-			<nav className="fixed backdrop-blur-sm bg-black/30 w-full p-4">
+			<nav className="fixed backdrop-blur-sm bg-black/30 w-full p-4 z-10">
 				<ul>
 					<li className="inline m-4">
 						<Link href="/#home" className="m-4">
