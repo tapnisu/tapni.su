2885b736a591271dc8cb4c37cc8f693660226690
Alexiy Rybin <0xtapnisu@gmail.com>
2022-08-20 14:56:00 +0700

    Added animations

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,50 @@
+import { AnimatePresence, domAnimation, LazyMotion, m } from "framer-motion";
+import type { AppProps } from "next/app";
 import "../styles/globals.css";
 
-import type { AppProps } from "next/app";
+const pages = [
+	{ href: "/", name: "Index" },
+	{ href: "/page-one", name: "One" },
+	{ href: "/page-two", name: "Two" },
+	{ href: "/page-three", name: "Three" },
+	{ href: "/page-four", name: "Four" }
+];
 
-function MyApp({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+function MyApp({ Component, pageProps, router }: AppProps) {
+	return (
+		<div className="app-wrap bg-black">
+			<LazyMotion features={domAnimation}>
+				<AnimatePresence exitBeforeEnter={true}>
+					<m.div
+						key={router.route.concat("Fade Back")}
+						className="page-wrap"
+						initial="initial"
+						animate="animate"
+						exit="exit"
+						variants={{
+							initial: {
+								opacity: 0,
+								scale: 0.7
+							},
+							animate: {
+								opacity: 1,
+								scale: 1
+							},
+							exit: {
+								opacity: 0,
+								scale: 0.7
+							}
+						}}
+						transition={{
+							duration: 0.7
+						}}
+					>
+						<Component {...pageProps} />
+					</m.div>
+				</AnimatePresence>
+			</LazyMotion>
+		</div>
+	);
 }
 
 export default MyApp;
