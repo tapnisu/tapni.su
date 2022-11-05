4f8232a25c0f6e890c3715b1a3eeb24100a44406
Alexiy Rybin <0xtapnius@gmail.com>
2022-11-05 19:27:39 +0700

    Fix

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 const Navbar = () => {
 	return (
 		<>
-			<nav className="fixed backdrop-blur-sm bg-neutral-700/30 text-white w-full">
+			<nav className="fixed backdrop-blur-sm bg-neutral-800/30 text-white w-full">
 				<ul>
 					<li className="inline-block m-4">
 						<Link href="/" className="m-4">
