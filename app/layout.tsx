3177fd387ba00d1c6565ed59aca423d33a62e713
Alexiy Rybin <0xtapnisu@gmail.com>
2022-11-05 21:28:50 +0700

    Fix for phones

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
 				/>
 				<meta name="description" content="My personal website!" />
 				<link rel="icon" href="/favicon.ico" />
+				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</head>
 
 			<body className="bg-black text-white">
