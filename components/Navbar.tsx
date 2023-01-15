a94445f54502296b6726a03c7552489412b82f59
Alexiy Rybin <0xtapnisu@gmail.com>
2023-01-15 10:58:58 +0700

    change navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,24 +1,26 @@
 const Navbar = () => {
   return (
     <>
-      <nav className="fixed bg-black text-white w-full">
-        <ul>
-          <li className="inline-block m-4">
-            <a href="/" className="m-4">
-              Home
-            </a>
-          </li>
-          <li className="inline-block m-4">
-            <a href="/projects" className="m-4">
-              Projects
-            </a>
-          </li>
-          <li className="inline-block m-4">
-            <a href="/contact" className="m-4">
-              Contact
-            </a>
-          </li>
-        </ul>
+      <nav className="fixed flex justify-center w-full">
+        <div className="bg-gray-900 text-white rounded-full m-4">
+          <ul>
+            <li className="inline-block m-4">
+              <a href="/" className="m-4">
+                Home
+              </a>
+            </li>
+            <li className="inline-block m-4">
+              <a href="/projects" className="m-4">
+                Projects
+              </a>
+            </li>
+            <li className="inline-block m-4">
+              <a href="/contact" className="m-4">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </>
   );
