c4db1a47698555b47b48618b79f37528acf490b2
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-12 14:03:29 +0700

    check source code

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,7 +21,7 @@ const Footer = () => {
         </ul>
       </div>
 
-      <h3 className="p-4 text-gray-400">Copyright © 2023 Alexiy Rybin</h3>
+      <h3 className="p-4 text-gray-400">© 2023 Alexiy Rybin. All rights reserved.<br/><a href="https://github.com/tapnisu/website" className="font-mono italic">Check website's source code {"<-"}</a></h3>
     </footer>
   );
 };
