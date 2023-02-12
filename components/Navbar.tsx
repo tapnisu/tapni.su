b82e2fdea7458f11ff0337b9f83e34f0ee5685bc
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-12 15:54:45 +0700

    move navbar into array

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,27 +4,20 @@ const Navbar = () => {
       <nav className="sticky top-0 flex justify-center w-full">
         <div className="bg-gray-900 text-white rounded-full m-2 p-1">
           <ul>
-            <li className="inline-block m-2 mx-4">
-              <a href="/" className="hover:text-indigo-400 transition-colors">
-                Home
-              </a>
-            </li>
-            <li className="inline-block m-2 mx-4">
-              <a
-                href="/projects"
-                className="hover:text-indigo-400 transition-colors"
-              >
-                Projects
-              </a>
-            </li>
-            <li className="inline-block m-2 mx-4">
-              <a
-                href="/setup"
-                className="hover:text-indigo-400 transition-colors"
-              >
-                My tools
-              </a>
-            </li>
+            {[
+              { name: "Home", url: "/" },
+              {
+                name: "Projects",
+                url: "/projects",
+              },
+              { name: "Setup", url: "/setup" },
+            ]?.map((info: { name: string; url: string }) => (
+              <li className="inline-block m-2 mx-4">
+                <a href="/" className="hover:text-indigo-400 transition-colors">
+                  {info.name}
+                </a>
+              </li>
+            ))}
           </ul>
         </div>
       </nav>
