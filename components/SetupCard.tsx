ca0cf3b90d0429a30989d87dcd851d0708b18e67
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-12 14:15:11 +0700

    remove on hover setup card

--- components/SetupCard.tsx
+++ components/SetupCard.tsx
@@ -13,7 +13,7 @@ const SetupCard = ({
 }: SetupCardProps) => {
   return (
     <>
-      <div className="bg-gray-900 hover:bg-gray-800 transition-colors m-2 p-4 rounded-3xl">
+      <div className="bg-gray-900 m-2 p-4 rounded-3xl">
         {imageURL && (
           <img src={imageURL} alt={imageAlt} class="w-full rounded-3xl p-2" />
         )}
