40ddce263782b707424833290dcf3c2879bed7bf
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-12 15:52:12 +0700

    move footer into array

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,43 +4,30 @@ const Footer = () => {
       <div>
         <h2 className="text-xl">Social media</h2>
         <ul className="p-4 list-circle">
-          <li>
-            <a
-              href="https://github.com/tapnisu"
-              className="hover:text-indigo-400 transition-colors"
-            >
-              Github
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://discord.com/users/586128640136445964"
-              className="hover:text-indigo-400 transition-colors"
-            >
-              Discord: (tapnisu#7384)
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://twitter.com/tapnisu"
-              className="hover:text-indigo-400 transition-colors"
-            >
-              Twitter
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://t.me/tapnisu"
-              className="hover:text-indigo-400 transition-colors"
-            >
-              Telegram
-            </a>
-          </li>
+          {[
+            { title: "Github: tapnisu", url: "https://github.com/tapnisu" },
+            {
+              title: "Discord: (tapnisu#7384)",
+              url: "https://discord.com/users/586128640136445964",
+            },
+            { title: "Twitter: @tapnisu", url: "https://twitter.com/tapnisu" },
+            { title: "Telegram: @tapnisu", url: "https://t.me/tapnisu" },
+          ]?.map((info: { title: string; url: string }) => (
+            <li>
+              <a
+                href={info.url}
+                className="hover:text-indigo-400 transition-colors"
+              >
+                {info.title}
+              </a>
+            </li>
+          ))}
         </ul>
       </div>
 
       <h3 className="p-4 text-gray-400">
-        © 2023 Alexiy Rybin. All rights reserved.<br />
+        © 2023 Alexiy Rybin. All rights reserved.
+        <br />
         <a
           href="https://github.com/tapnisu/website"
           className="font-mono italic hover:text-indigo-400 transition-colors"
