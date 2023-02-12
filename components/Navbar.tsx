d393490837e3b1e49f9cb0c8079ddf01dbea20dc
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-12 13:17:58 +0700

    Smooth navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,13 +5,25 @@ const Navbar = () => {
         <div className="bg-gray-900 text-white rounded-full m-2 p-1">
           <ul>
             <li className="inline-block m-2 mx-4">
-              <a href="/">Home</a>
+              <a href="/" className="hover:text-indigo-400 transition-colors">
+                Home
+              </a>
             </li>
             <li className="inline-block m-2 mx-4">
-              <a href="/projects">Projects</a>
+              <a
+                href="/projects"
+                className="hover:text-indigo-400 transition-colors"
+              >
+                Projects
+              </a>
             </li>
             <li className="inline-block m-2 mx-4">
-              <a href="/setup">My tools</a>
+              <a
+                href="/setup"
+                className="hover:text-indigo-400 transition-colors"
+              >
+                My tools
+              </a>
             </li>
           </ul>
         </div>
