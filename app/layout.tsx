49757b31530a2c0e0365172c81885b88d78483c5
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-21 20:00:13 +0700

    Add icon type

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<link rel="icon" href="/favicon.ico" />
+				<link rel="icon" type="image/x-icon" href="/favicon.ico" />
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 			</head>
 
