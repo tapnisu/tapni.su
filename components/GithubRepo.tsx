004333ea46dc218494c24e90a7a448b48a83e5b0
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-21 18:43:34 +0700

    Fix gray color

--- components/GithubRepo.tsx
+++ components/GithubRepo.tsx
@@ -20,7 +20,7 @@ const GithubRepo = ({
 	issues
 }: GithubRepoProps) => {
 	return (
-		<div className="bg-gray-100 dark:bg-neutral-900 m-2 p-8 rounded-3xl border-2 border-gray-200 dark:border-neutral-800">
+		<div className="bg-neutral-100 dark:bg-neutral-900 m-2 p-8 rounded-3xl border-2 border-neutral-200 dark:border-neutral-800">
 			<Link href={url}>
 				<h2 className="text-xl cursor-pointer text-black hover:text-indigo-600 dark:text-white dark:hover:text-indigo-400 transition-colors">
 					{name}
