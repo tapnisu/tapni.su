895aeb68623aadd59c5d359f57a9b19e5dc97cb1
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-21 18:08:14 +0700

    Update footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,27 +1,40 @@
-import Link from "next/link";
-
 const Footer = () => {
 	return (
-		<footer className="bg-gradient-to-b from-transparent to-neutral-900 flex flex-col items-center p-8">
-			<div className="mx-auto w-24 h-1 my-12 bg-white rounded-full"></div>
-			<h2 className="text-xl">Social media</h2>
-			<ul className="list-disc p-4">
-				<li>
-					<Link href="https://github.com/tapnisu">Github</Link>
-				</li>
-				<li>
-					<Link href="https://discord.com/users/586128640136445964">
-						Discord (tapnisu#7384)
-					</Link>
-				</li>
-				<li>
-					<Link href="https://twitter.com/tapnisu">Twitter</Link>
-				</li>
-				<li>
-					<Link href="https://t.me/tapnisu">Telegram</Link>
-				</li>
-			</ul>
-			<h3 className="p-4">Copyright © 2022 Alexiy Rybin</h3>
+		<footer className="text-neutral-800 dark:text-neutral-400 bg-gradient-to-b from-transparent to-indigo-300 dark:to-neutral-900 flex flex-row flex-wrap items-center justify-around p-8 w-full">
+			<div>
+				<h2 className="text-xl">Social media</h2>
+				<ul className="p-4 list-disc">
+					{[
+						{ name: "Github: tapnisu", url: "https://github.com/tapnisu" },
+						{
+							name: "Discord: (tapnisu#7384)",
+							url: "https://discord.com/users/586128640136445964"
+						},
+						{ name: "Twitter: @tapnisu", url: "https://twitter.com/tapnisu" },
+						{ name: "Telegram: @tapnisu", url: "https://t.me/tapnisu" }
+					]?.map((info: { name: string; url: string }) => (
+						<li key={info.name}>
+							<a
+								href={info.url}
+								className="hover:text-indigo-600 hover:dark:text-indigo-400 transition-colors"
+							>
+								{info.name}
+							</a>
+						</li>
+					))}
+				</ul>
+			</div>
+
+			<h3 className="p-4">
+				© 2023 Alexiy Rybin. All rights reserved.
+				<br />
+				<a
+					href="https://github.com/tapnisu/website"
+					className="font-mono italic hover:text-indigo-600 hover:dark:text-indigo-400 transition-colors"
+				>
+					Check website{"'"}s source code {"<-"}
+				</a>
+			</h3>
 		</footer>
 	);
 };
