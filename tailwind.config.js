6ecb3b4cecc7733de256654b370a10bbe6bee6ff
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-21 18:07:48 +0700

    Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,18 +3,7 @@ module.exports = {
 	content: [
 		"./app/**/*.{js,ts,jsx,tsx}",
 		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}"
 	],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["JetBrains Mono"],
-				serif: ["JetBrains Mono"],
-				mono: ["JetBrains Mono"],
-				display: ["JetBrains Mono"],
-				body: ["JetBrains Mono"],
-			},
-		},
-	},
-	plugins: [],
+	plugins: []
 };
