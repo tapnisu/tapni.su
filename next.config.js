2779ef972ca8eba3503bc5f569d111499d92d7e8
Alexiy Rybin <0xtapnisu@gmail.com>
2023-02-21 18:18:13 +0700

    It is not private

--- next.config.js
+++ next.config.js
@@ -3,6 +3,31 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	experimental: { appDir: true },
+	async redirects() {
+		return [
+			{
+				source: "/skyline",
+				destination: "/api/skyline",
+				permanent: true
+			},
+			{
+				source: "/cxsmxs",
+				destination: "https://cxsmxs.space/",
+				permanent: true
+			},
+			{
+				source: "/telegram",
+				destination: "https://t.me/yurionblog",
+				permanent: true
+			},
+			{
+				source: "/discord",
+				destination: "https://discord.gg/JkXFQpScFj",
+				permanent: true
+			}
+		];
+	},
+	output: "standalone"
 };
 
 module.exports = nextConfig;
