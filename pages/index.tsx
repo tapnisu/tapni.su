18a86f38406ee27169be1f64a8809dff8c0f4ccb
Alexiy Rybin <0xtapnisu@gmail.com>
2023-03-19 10:19:28 +0700

    Move my projects to main page

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const HomePage = () => {
 			title="Tapnisu - Home"
 			description="Website of 15 years old developer from Russia"
 		>
-			<div className="flex flex-col items-center">
+			<div className="flex flex-col items-center justify-center min-h-[50vh]">
 				<div className="flex flex-row flex-wrap justify-center items-center p-2">
 					<Image
 						src={Avatar}
@@ -25,10 +25,7 @@ const HomePage = () => {
 					<div className="flex flex-col flex-wrap items-center justify-center p-2 h-full">
 						<h1 className="text-3xl font-bold">
 							Oh, hi! I am{" "}
-							<span className="text-accent-500 font-mono">
-								Tapnisu
-							</span>
-							!
+							<span className="text-accent-500 font-mono">Tapnisu</span>!
 						</h1>
 
 						<p>
@@ -49,6 +46,74 @@ const HomePage = () => {
 							<Button>Checkout projects</Button>
 						</Link>
 					</div>
+				</div>
+			</div>
+			<div className="flex flex-col justify-between items-center">
+				<div className="p-2">
+					<h1 className="text-3xl font-bold p-2">My projects</h1>
+
+					<ul className="list-inside">
+						{[
+							{
+								name: "tapciify",
+								url: "https://github.com/tapnisu/tapciify",
+								description:
+									"CLI tool that can let you view images in terminal",
+								language: "Rust"
+							},
+							{
+								name: "tapris",
+								url: "https://github.com/tapris-bot/tapris",
+								description:
+									"Tapris rewrite with Deno and @harmonyland/harmony",
+								language: "TypeScript"
+							},
+							{
+								name: "forwarding-discord-telegram",
+								url: "https://github.com/tapnisu/forwarding-discord-telegram",
+								description:
+									"This bot can help you to read discord messages in telegram.",
+								language: "JavaScript"
+							},
+							{
+								name: "exhrtcc",
+								url: "https://github.com/tapnisold/exhrtcc",
+								description:
+									"CLI tool that can let you to convert currencies without leaving your terminal",
+								language: "Rust"
+							},
+							{
+								name: "tapris-node",
+								url: "https://github.com/tapris-bot/tapris-node",
+								description:
+									"Discord.js 14 bot made in TypeScript (currently tapris)",
+								language: "TypeScript"
+							}
+						]?.map(
+							(repo: {
+								name: string;
+								url: string;
+								description: string;
+								language: string;
+							}) => (
+								<>
+									<li key={repo.name} className="list-disc">
+										<Link
+											href={repo.url}
+											className="text-xl cursor-pointer text-black hover:text-main-600 dark:text-main-100 dark:hover:text-main-400 transition-colors"
+										>
+											{repo.name}
+										</Link>
+									</li>
+									<p className="text-main-800 dark:text-main-400">
+										{repo.description}
+										<br />
+										Written in {repo.language}
+									</p>
+								</>
+							)
+						)}
+					</ul>
 				</div>
 			</div>
 		</BasePageLayout>
