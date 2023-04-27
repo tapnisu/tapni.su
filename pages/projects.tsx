f7fa5c0dab518b82ae0ba22f8f657897cb0f8387
Alexiy Rybin <0xtapnisu@gmail.com>
2023-04-27 21:50:19 +0700

    Make projects and setup prettier

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -8,62 +8,60 @@ const ProjectsPage = () => {
 		<BasePageLayout title="Tapnisu - Projects" description="Projects I made">
 			<h1 className="text-3xl font-bold p-2">My projects</h1>
 
-			<div>
-				{[
-					{
-						name: "tapciify",
-						image: tapciifyImage,
-						url: "https://github.com/tapnisu/tapciify",
-						description:
-							"CLI tool that can let you view images/videos in terminal as ASCII",
-						language: "Rust"
-					},
-					{
-						name: "tapris",
-						url: "https://github.com/tapris-bot/tapris",
-						description:
-							"Lightweight multi-language, multi-purpose bot for Discord made with Deno",
-						language: "TypeScript"
-					},
-					{
-						name: "forwarding-discord-telegram",
-						url: "https://github.com/tapnisu/forwarding-discord-telegram",
-						description:
-							"Bot that can help you to read discord messages in telegram",
-						language: "JavaScript"
-					},
-					{
-						name: "exhrtcc",
-						url: "https://github.com/tapnisold/exhrtcc",
-						description:
-							"A CLI tool that allows you to convert currencies without leaving the terminal",
-						language: "Rust"
-					},
-					{
-						name: "tapris-node",
-						url: "https://github.com/tapris-bot/tapris-node",
-						description:
-							"Discord.js 14 bot made in TypeScript (currently tapris)",
-						language: "TypeScript"
-					},
-					{
-						name: "website",
-						image: websiteImage,
-						url: "https://github.com/tapnisu/website",
-						description: "My personal website (you are here right now)",
-						language: "Typescript"
-					}
-				]?.map((info) => (
-					<Card
-						title={info.name}
-						image={info.image}
-						url={info.url}
-						description={info.description}
-						comments={`Written in ${info.language}`}
-						key={info.name}
-					/>
-				))}
-			</div>
+			{[
+				{
+					name: "tapciify",
+					image: tapciifyImage,
+					url: "https://github.com/tapnisu/tapciify",
+					description:
+						"CLI tool that can let you view images/videos in terminal as ASCII",
+					language: "Rust"
+				},
+				{
+					name: "tapris",
+					url: "https://github.com/tapris-bot/tapris",
+					description:
+						"Lightweight multi-language, multi-purpose bot for Discord made with Deno",
+					language: "TypeScript"
+				},
+				{
+					name: "forwarding-discord-telegram",
+					url: "https://github.com/tapnisu/forwarding-discord-telegram",
+					description:
+						"Bot that can help you to read discord messages in telegram",
+					language: "JavaScript"
+				},
+				{
+					name: "exhrtcc",
+					url: "https://github.com/tapnisold/exhrtcc",
+					description:
+						"A CLI tool that allows you to convert currencies without leaving the terminal",
+					language: "Rust"
+				},
+				{
+					name: "tapris-node",
+					url: "https://github.com/tapris-bot/tapris-node",
+					description:
+						"Discord.js 14 bot made in TypeScript (currently tapris)",
+					language: "TypeScript"
+				},
+				{
+					name: "website",
+					image: websiteImage,
+					url: "https://github.com/tapnisu/website",
+					description: "My personal website (you are here right now)",
+					language: "Typescript"
+				}
+			]?.map(({ name, image, url, description, language }) => (
+				<Card
+					title={name}
+					image={image}
+					url={url}
+					description={description}
+					comments={`Written in ${language}`}
+					key={name}
+				/>
+			))}
 		</BasePageLayout>
 	);
 };
--- pages/setup.tsx
+++ pages/setup.tsx
@@ -9,30 +9,24 @@ const SetupPage = () => {
 		>
 			<h1 className="text-3xl font-bold">My setup</h1>
 
-			<div>
-				{[
-					{
-						name: "PC",
-						description:
-							"My PC, standing on a chair with Intel Core i9 11900KF, 64GB RAM (16GB*4) and RTX 3080 12GB in it"
-					},
-					{
-						name: "Keychron K2 V2",
-						description:
-							"Keyboard I use for coding. Gateron Red Switches and toilet paper modded"
-					},
-					{
-						name: "Razer Viper V2 Pro",
-						description: "Mouse"
-					}
-				].map((info) => (
-					<Card
-						title={info.name}
-						description={info.description}
-						key={info.name}
-					/>
-				))}
-			</div>
+			{[
+				{
+					name: "PC",
+					description:
+						"My PC, standing on a chair with Intel Core i9 11900KF, 64GB RAM (16GB*4) and RTX 3080 12GB in it"
+				},
+				{
+					name: "Keychron K2 V2",
+					description:
+						"Keyboard I use for coding. Gateron Red Switches and toilet paper modded"
+				},
+				{
+					name: "Razer Viper V2 Pro",
+					description: "Mouse"
+				}
+			].map(({ name, description }) => (
+				<Card title={name} description={description} key={name} />
+			))}
 		</BasePageLayout>
 	);
 };
