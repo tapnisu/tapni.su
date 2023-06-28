fe597c5cf9dc1de6ca5779d85112b3be169f61e8
Alexiy Rybin <0xtapnisu@gmail.com>
2023-06-28 15:26:19 +0700

    Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,18 @@
 import Link from "next/link";
 
+const paths = [
+	{ name: "Home", url: "/" },
+	{
+		name: "Projects",
+		url: "/projects"
+	},
+	{ name: "Setup", url: "/setup" }
+];
+
 const Navbar = () => (
 	<nav className="sticky top-0 flex font-semibold justify-center md:justify-normal w-full text-accent-600 bg-main-50/75 dark:bg-main-950/75 backdrop-blur-sm p-2">
 		<ul>
-			{[
-				{ name: "Home", url: "/" },
-				{
-					name: "Projects",
-					url: "/projects"
-				},
-				{ name: "Setup", url: "/setup" }
-			]?.map((info: { name: string; url: string }) => (
+			{paths?.map((info: { name: string; url: string }) => (
 				<li
 					className="inline-block py-2 rounded-full hover:bg-accent-600 hover:text-main-100 transition"
 					key={info.name}
