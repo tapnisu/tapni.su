d765c08263931ba81c3115896472132572787d69
Alexiy Rybin <0xtapnisu@gmail.com>
2023-07-30 07:44:41 +0700

    Alexiy Rybin

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,19 +10,22 @@ const paths = [
 ];
 
 const Navbar = () => (
-	<nav className="sticky top-0 flex font-semibold justify-center md:justify-normal w-full text-accent-600 bg-main-50/75 dark:bg-main-950/75 backdrop-blur-sm p-2">
-		<ul>
+	<nav className="sticky top-0 font-semibold flex flex-wrap justify-around w-full text-accent-600 bg-main-50/75 dark:bg-main-950/75 backdrop-blur-sm p-2">
+		<div className="py-2 rounded-full transition hidden md:inline-block">
+			<div className="px-4">Alexiy Rybin</div>
+		</div>
+		<div>
 			{paths?.map((info: { name: string; url: string }) => (
-				<li
+				<div
 					className="inline-block py-2 rounded-full hover:bg-accent-600 hover:text-main-100 transition"
 					key={info.name}
 				>
 					<Link className="p-2 px-4" href={info.url}>
 						{info.name}
 					</Link>
-				</li>
+				</div>
 			))}
-		</ul>
+		</div>
 	</nav>
 );
 
