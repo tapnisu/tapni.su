0158e021a90f0cbfc1b94bc10ab6439277a39855
Alexiy Rybin <0xtapnisu@gmail.com>
2023-08-15 19:04:24 +0700

    index

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,18 +4,14 @@ import Link from "next/link";
 const HomePage = () => {
   return (
     <BasePageLayout>
-      <div className="flex flex-col flex-wrap justify-center items-center">
-        <div className="flex flex-col flex-wrap items-center justify-center p-2 h-full">
-          <h1 className="text-3xl font-bold">
-            Oh, hi! I am <span className="text-accent-600">tapnisu</span>!
-          </h1>
+      <div className="container-center">
+        <h1>
+          Oh, hi! I am <span className="color-accent">tapnisu</span>!
+        </h1>
 
-          <div className="m-4">
-            <Link href="/contact">
-              <button>Contact me</button>
-            </Link>
-          </div>
-        </div>
+        <Link href="/contact">
+          <button>Contact me</button>
+        </Link>
       </div>
     </BasePageLayout>
   );
