8e40381cb83563599281f15a020968e4fb5714cc
Alexiy Rybin <0xtapnisu@gmail.com>
2023-08-15 18:00:23 +0700

    Css navbar

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -13,16 +13,13 @@ const paths = [
 export default function Navbar() {
   return (
     <nav className={styles.navbar}>
-      <div className="py-2 rounded-full transition hidden md:inline-block">
-        <div className="px-4">Alexiy Rybin</div>
+      <div className={styles.title}>
+        <div className={styles.linkName}>Alexiy Rybin</div>
       </div>
       <div>
         {paths?.map((info: { name: string; url: string }) => (
-          <div
-            className="inline-block py-2 rounded-full hover:bg-accent-600 hover:text-main-100 transition"
-            key={info.name}
-          >
-            <Link className="p-2 px-4" href={info.url}>
+          <div className={styles.link} key={info.name}>
+            <Link className={styles.linkName} href={info.url}>
               {info.name}
             </Link>
           </div>
