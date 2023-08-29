e532c34cfd09d588f8f563ba4d8eceda3e3868ad
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-08-29 14:04:41 +0700

    move headers from body

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "@styles/globals.css";
 import type { AppProps } from "next/app";
 import { Inter } from "next/font/google";
-import Head from "next/head";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -9,13 +8,8 @@ const inter = Inter({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
-    </>
+    <main className={inter.className}>
+      <Component {...pageProps} />
+    </main>
   );
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,15 +4,13 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Language" content="en" />
-
         <meta name="theme-color" content="#000000" />
-
         <link rel="icon" type="image/x-icon" href="/favicon.ico" />
         <meta name="og:image" content="https://tapnisu.ru/og.png" />
-
         <meta name="og:url" content="https://tapnisu.ru/" />
-
         <meta name="twitter:image" content="https://tapnisu.ru/og.png" />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@tapnisu" />
