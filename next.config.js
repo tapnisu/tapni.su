a51cc85b7f9bf4b2adcea4fac487698f0e971504
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-09-11 22:07:20 +0700

    Use preact

--- next.config.js
+++ next.config.js
@@ -1,11 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+        react: "preact/compat",
+        "react-dom/test-utils": "preact/test-utils",
+        "react-dom": "preact/compat",
+      });
+    }
+    return config;
+  },
+
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ru"],
   },
-  reactStrictMode: true,
-  swcMinify: true,
+
   async redirects() {
     return [
       {
