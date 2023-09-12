42c64915565cfa97e2af6c7526549a182acfdef7
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-09-12 19:46:32 +0700

    Nah, i am good without preact

--- next.config.js
+++ next.config.js
@@ -3,18 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
-    return config;
-  },
-
   i18n: {
     defaultLocale: "en",
     locales: ["en", "ru"],
