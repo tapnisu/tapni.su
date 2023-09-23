2624a7041785499824fe59c9c6d7a5f9e8ce59b1
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-09-23 12:19:47 +0700

    Added cookie count saving

--- pages/cookie/index.tsx
+++ pages/cookie/index.tsx
@@ -1,15 +1,26 @@
 import BasePageLayout from "@components/basePageLayout";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styles from "./cookie.module.css";
 
 export default function CookiePage() {
   const [cookies, setCookies] = useState(0);
 
+  function AddCookie() {
+    const newCookies = cookies + 1;
+
+    setCookies(newCookies);
+    localStorage.setItem("cookieCount", newCookies.toString());
+  }
+
+  useEffect(() => {
+    setCookies(Number(localStorage.getItem("cookieCount")) || 0);
+  }, []);
+
   return (
     <BasePageLayout title="Cookie">
       <div className="text-center">
         <b>{cookies}</b>
-        <div className={styles.cookie} onClick={() => setCookies(cookies + 1)}>
+        <div className={styles.cookie} onClick={AddCookie}>
           🍪
         </div>
       </div>
