e970735e26ef7b4753712e120ccccbadb3c1fd95
Aleksei Rybin <0xalekseirybin@gmail.com>
2023-10-07 16:53:10 +0700

    changed sitemap change freq

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,8 @@ const config = {
   siteUrl: process.env.SITE_URL || "https://tapni.su",
   generateRobotsTxt: true,
   sitemapSize: 7000,
-  changefreq: "daily",
+  changefreq: "weekly",
+
   transform: async (config, path) => {
     return {
       loc: path.replace(".en-US", ""),
