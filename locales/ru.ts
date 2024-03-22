56b1c20c757ba717a47557b9fc3a7c7d832bb922
Aleksei Rybin <0xalekseirybin@gmail.com>
2024-03-22 15:15:07 +0700

    Change "Блог" into "Telegram канал"

--- locales/ru.ts
+++ locales/ru.ts
@@ -8,7 +8,7 @@ export const ru = {
   contactTitle: "Связаться",
   aboutTitle: "Обо мне",
 
-  blog: "Блог (в основном на английском)",
+  blog: "Telegram канад",
 
   themes: { system: "Системная", light: "Светлая", dark: "Тёмная" },
 
