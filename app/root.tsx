423c0264269ced8d41066f21ad16325081affc49
Aleksei Rybin <aleksei@tapni.su>
2024-10-05 23:14:51 +0700

    Revert previous commit

--- app/root.tsx
+++ app/root.tsx
@@ -33,8 +33,6 @@ export const links: LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
-  { rel: "preload", as: "json", href: "/locales/en/common.json" },
-  { rel: "preload", as: "json", href: "/locales/ru/common.json" },
 ];
 
 export async function loader({ request }: LoaderFunctionArgs) {
